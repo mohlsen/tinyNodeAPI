2825c39577f87303d770c9d1d3ee8a0281036a90
Mike Ohlsen <mjohlsen@ra.rockwell.com>
2014-01-27 22:47:53 -0500

    added back CORS support

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var app = require('express.io')(),
 app.http().io();
 
 app.all('*', function(req, res, next) {
-  //res.header("Access-Control-Allow-Origin", "*");
-  //res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
  });
 
